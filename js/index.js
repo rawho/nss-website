dbc98d10faff72d028e5e51b2d9f18bb4e111fcf
Rahul T <rahulmanoj@cet.ac.in>
2020-11-10 10:14:19 +0530

    nav toggler

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.getElementById("hamburger-menu")
+const hamburgerClose = document.getElementById("hamburger-close")
+
+
+hamburgerMenu.addEventListener('click', () => {
+    document.querySelector(".navbar").classList.add('hidden')
+    document.querySelector(".navbar-toggle").classList.remove("hidden")
+})
+
+hamburgerClose.addEventListener('click', () => {
+    document.querySelector(".navbar").classList.remove('hidden')
+    document.querySelector(".navbar-toggle").classList.add("hidden")
+})
